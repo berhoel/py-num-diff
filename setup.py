ffff123b53574cfbb4bfd36ff38727aa6e7411e6
Berthold Höllmann <berthold.hoellmann@dnvgl.com>
2012-10-31 13:47:22 +0000

    enable generation of EGG installer (GLPY-102)

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ __docformat__ = "restructuredtext en"
 
 import os.path
 import re
-from distutils.core import setup
+from setuptools import setup
 from distutils.command import build_scripts
 
 build_scripts.first_line_re = re.compile(r'^###!.*python(\s+.*)?$')
@@ -25,11 +25,21 @@ build_scripts.first_line_re = re.compile(r'^###!.*python(\s+.*)?$')
 setup(name='numdiff',
       version='1.1',
       description="Numerical diff, written in Python",
-      author=u'B. Höllmann',
+      keywords="diff numerical compare",
+      author=u'Berthold Höllmann',
       author_email="hoel@GL-Group.com",
+      license='Other/Proprietary License',
       package_dir={'': 'lib'},
       packages=['numdiff'],
       scripts=[os.path.join('app', 'numdiff')],
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Topic :: Utilities",
+          "Environment :: Win32 (MS Windows)",
+          "Environment :: Linux",
+          "Intended Audience :: End Users/Desktop",
+          "License :: Other/Proprietary License",
+          "Topic :: Scientific/Engineering",]
       )
 
 # Local Variables:
